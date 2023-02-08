78f73b77f76f2cbf434a660a19e142702b037310
Pramesh10 <kcpramesh444@gmail.com>
2023-02-08 22:30:06 +0545

    Add project files.

---
+++ FinalProjects/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FinalProjects.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
